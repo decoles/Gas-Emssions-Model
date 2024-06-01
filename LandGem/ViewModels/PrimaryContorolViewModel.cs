03e129b88601a29c26713bf2da6dc08537e945e2
David Coles <davidepcoles@gmail.com>
2024-06-01 15:20:48 -0700

    new commit

--- LandGem/ViewModels/PrimaryContorolViewModel.cs
+++ LandGem/ViewModels/PrimaryContorolViewModel.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using LandGEM.Services;
+using LandGEM.Views;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace LandGEM.ViewModels
 {
-    public class PrimaryContorolViewModel
+    public class PrimaryContorolViewModel : ObservableObject
     {
-        public PrimaryContorolViewModel() { }
+        public PrimaryContorolViewModel() {
+
+        }
     }
 }
